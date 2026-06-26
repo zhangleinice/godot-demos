2ed4737d501951bff125835d7aa738ce0e3ab9da
rocky <963754365@qq.com>
2026-06-26 18:29:55 +0800

    feat: dodge-the-creeps

---
+++ dodge-the-creeps/Player.cs
@@ -0,0 +1,51 @@
+using Godot;
+
+public partial class Player : Area2D
+{
+	[Export]
+	public int Speed { get; set; } = 400;
+
+	public Vector2 ScreenSize;
+
+	public override void _Ready()
+	{
+		ScreenSize = GetViewportRect().Size;
+	}
+
+	public override void _Process(double delta)
+	{
+		var velocity = Vector2.Zero;
+
+		if (Input.IsActionPressed("move_right"))
+			velocity.X += 1;
+
+		if (Input.IsActionPressed("move_left"))
+			velocity.X -= 1;
+
+		if (Input.IsActionPressed("move_down"))
+			velocity.Y += 1;
+
+		if (Input.IsActionPressed("move_up"))
+			velocity.Y -= 1;
+
+		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+
+		if (velocity.Length() > 0)
+		{
+			velocity = velocity.Normalized() * Speed;
+			animatedSprite2D.Play();
+		}
+		else
+		{
+			animatedSprite2D.Stop();
+		}
+
+		Position += velocity * (float)delta;
+		Position = new Vector2(
+			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
+			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
+		);
+
+
+	}
+}
